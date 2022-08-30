350b424e23c6ec8bc9f511e34b97581416b86904
Pope-dotcom <theiosemma630@gmail.com>
2022-08-30 14:08:38 +0100

    initial commit

---
+++ js EMMANUEL.js
@@ -0,0 +1,16 @@
+var m = 5;
+console.log(m)
+
+//var x = 6
+console.log(m)
+let g = 5;
+console.log(g)
+
+//var x = 6
+let x = 2
+let p = 7
+let h = x + p
+console.log(h)
+let a = 5;
+let b = 10
+console.log(a <= b)
